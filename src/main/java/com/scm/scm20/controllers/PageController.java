41cb43ab61162da5ad3027d6588916f923eab97b
hk2121 <hk2121999@gmail.com>
2024-10-11 15:03:06 +0530

    added some html pages & APIs

--- src/main/java/com/scm/scm20/controllers/PageController.java
+++ src/main/java/com/scm/scm20/controllers/PageController.java
@@ -2,6 +2,7 @@ package com.scm.scm20.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,25 +12,42 @@ public class PageController {
     public String home(Model model) {
         System.out.println("Home Page Handler");
 
-        //sending data the view
+        // sending data the view
         model.addAttribute("name", "SubString Technologies");
         model.addAttribute("YoutubeChannel", "Tech Freak");
         model.addAttribute("githubRepo", "https://start.spring.io/");
         return "home";
     }
 
-    //about route 
+    // about route
     @RequestMapping("/about")
-    public String aboutPage(Model model){
-        model.addAttribute("isLogin",true);
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
         System.out.println("About Page Loading");
         return "about";
     }
 
-    //services
+    // services
     @RequestMapping("/services")
-    public String servicesPage(){
+    public String servicesPage() {
         System.out.println("Service Page Loading");
         return "services";
+    }
+
+    // contact page
+    @GetMapping("/contact")
+    public String contact() {
+        System.out.println("Conatct Page Loading");
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
