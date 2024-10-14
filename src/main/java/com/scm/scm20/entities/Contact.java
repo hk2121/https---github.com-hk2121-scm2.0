8e72e7822f52a9bd6cad14439b18eecc858d5937
hk2121 <hk2121999@gmail.com>
2024-10-14 20:14:52 +0530

    java enetities added.

---
+++ src/main/java/com/scm/scm20/entities/Contact.java
@@ -0,0 +1,25 @@
+package com.scm.scm20.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Contact {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String picture;
+    @Column(length = 10000)
+    private String description;
+    private boolean favorite = false;
+
+    private String websiteLink;
+    private String linkedInLink;
+    // private List<SocialLink> socialLink = new ArrayList<>();
+
+}
---
+++ src/main/java/com/scm/scm20/entities/SocialLink.java
@@ -0,0 +1,5 @@
+package com.scm.scm20.entities;
+
+public class SocialLink {
+
+}
