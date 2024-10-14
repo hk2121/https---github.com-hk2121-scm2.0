7a85558789b923584390f7642c33e806a9ec2b9b
hk2121 <hk2121999@gmail.com>
2024-10-14 14:17:41 +0530

    Login,signup&theme button added.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,66 @@
 console.log("Script Loaded");
+
+//change theme work
+let currentTheme = getTheme();
+
+//initial -->
+document.addEventListener("DOMContentLoaded",() =>{
+    changeTheme();
+});
+
+//TODO:
+function changeTheme() {
+  // set to web Page
+
+  changePageTheme(currentTheme, currentTheme);
+  document.querySelector("html").classList.add(currentTheme);
+
+  //set the listenter to change theme button
+  const changeThemeButton = document.querySelector("#theme_change_button");
+
+  changeThemeButton.querySelector("span").textContent =
+    currentTheme == "light" ? "Dark" : "Light";
+  const oldTheme = currentTheme;
+
+  changeThemeButton.addEventListener("click", (event) => {
+    console.log("change Theme Button clicked");
+
+    if (currentTheme === "dark") {
+      // theme light
+      currentTheme = "light";
+    } else {
+      // theme dark
+      currentTheme = "dark";
+    }
+
+    changePageTheme(currentTheme, oldTheme);
+  });
+}
+
+//set theme to localStorage
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+//get theme from localStorage
+function getTheme() {
+  let theme = localStorage.getItem("theme");
+  return theme ? theme : "light";
+}
+
+//CHANGE CURRENT THEME PAGE
+function changePageTheme(theme, oldTheme) {
+  // updates in local storage
+  setTheme(currentTheme);
+
+  //remove the current theme
+  document.querySelector("html").classList.remove(oldTheme);
+
+  //set the current theme
+  document.querySelector("html").classList.add(theme);
+
+  //change the text of button
+  document
+    .querySelector("#theme_change_button")
+    .querySelector("span").textContent = theme == "light" ? "Dark" : "Light";
+}
