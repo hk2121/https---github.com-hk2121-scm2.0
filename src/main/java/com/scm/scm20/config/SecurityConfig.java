4500f15733b91e82f19044b0b9c9f74494fe5a98
hk2121 <hk2121999@gmail.com>
2024-10-25 19:00:23 +0530

    Customize login page

--- src/main/java/com/scm/scm20/config/SecurityConfig.java
+++ src/main/java/com/scm/scm20/config/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import com.scm.scm20.services.impl.SecurityCustomUserDetailService;
 
 @Configuration
@@ -65,14 +63,44 @@ public class SecurityConfig {
             authorize.anyRequest().permitAll();
         });
 
-        //form default login
+        // form default login
         // need to change something then here we do .. related to form login
-        httpSecurity.formLogin(formLogin ->{
+        httpSecurity.formLogin(formLogin -> {
 
             formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/dashboard");
+           // formLogin.failureForwardUrl("login?error=true");
+            formLogin.defaultSuccessUrl("/home");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+            
+            // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+            // @Override
+            // public void onAuthenticationFailure(HttpServletRequest request,
+            // HttpServletResponse response,
+            // AuthenticationException exception) throws IOException, ServletException {
+            // // TODO Auto-generated method stub
+            // throw new UnsupportedOperationException("Unimplemented method
+            // 'onAuthenticationFailure'");
+            // }
+
+            // });
+
+            // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+            // @Override
+            // public void onAuthenticationSuccess(HttpServletRequest request,
+            // HttpServletResponse response,
+            // Authentication authentication) throws IOException, ServletException {
+            // // TODO Auto-generated method stub
+            // throw new UnsupportedOperationException("Unimplemented method
+            // 'onAuthenticationSuccess'");
+            // }
+
+            // });
         });
-
-
 
         return httpSecurity.build();
 
